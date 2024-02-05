5e7e26b5839b30c9248d42331c1fce6585607628
Iago <iagopmo@gmail.com>
2024-02-05 01:13:50 -0300

    criacao componente banner e imagens

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Meu primeiro projeto Vue!</h1>
+  <Banner />
 </template>
+
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner }
+}
+
+</script>
+
+<style>
+
+</style>
