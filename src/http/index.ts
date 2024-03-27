59d4b570ed081d36f271def5c2215c6cb4996953
Iago <iagopmo@gmail.com>
2024-03-27 14:45:49 -0300

    implementando pagina de receitas

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,22 @@
 import type ICategoria from "@/interfaces/ICategorias";
+import type IReceita from "@/interfaces/IReceita";
+
+async function obterDadosURL<T>(url: string) {
+    const resposta = await fetch(url);
+    return resposta.json() as T;
+}
 
 export async function obterCategorias() {
     // faz uma requisição get
-    const resposta = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
-    const categorias : ICategoria[] = await resposta.json();    
-    return categorias;
+    return obterDadosURL<ICategoria[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
+    // const resposta = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
+    // const categorias : ICategoria[] = await resposta.json();    
+    // return categorias;
+}
+
+export async function obterReceitas() {
+    return obterDadosURL<IReceita[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/receitas.json');
+    // const resposta = await fetch('');
+    // const receitas : IReceita[] = await resposta.json();
+    // return receitas;
 }
